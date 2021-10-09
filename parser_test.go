29a184a4d65be5960cf9a2da432231cb6dac93e7
Axetroy <axetroy.dev@gmail.com>
2021-10-09 22:18:49 +0800

    test: add test for paseFooterParagraph

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,7 @@
 package conventionalcommitparser
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -542,6 +543,13 @@ func Test_isFooterParagraph(t *testing.T) {
 			want: true,
 		},
 		{
+			name: "invalid hash footer paragraph with # prefix",
+			args: args{
+				txt: "#Close #1, #2",
+			},
+			want: false,
+		},
+		{
 			name: "invalid hash footer paragraph without # prefix",
 			args: args{
 				txt: "Close 1, #2",
@@ -567,6 +575,95 @@ func Test_isFooterParagraph(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := isFooterParagraph(tt.args.txt); got != tt.want {
 				t.Errorf("isFooterParagraph() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_paseFooterParagraph(t *testing.T) {
+	type args struct {
+		txt string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Footer
+	}{
+		{
+			name: "invalid footer paragraph",
+			args: args{
+				txt: "hello world",
+			},
+			want: Footer{Title: "hello world"},
+		},
+		{
+			name: "invalid footer paragraph",
+			args: args{
+				txt: "helloworld",
+			},
+			want: Footer{Title: "helloworld"},
+		},
+		{
+			name: "tag footer paragraph",
+			args: args{
+				txt: "Refs: #2",
+			},
+			want: Footer{Tag: "Refs", Title: "#2"},
+		},
+		{
+			name: "tag with - footer paragraph",
+			args: args{
+				txt: "Refs-With: #2",
+			},
+			want: Footer{Tag: "Refs-With", Title: "#2"},
+		},
+		{
+			name: "tag with multiple - footer paragraph",
+			args: args{
+				txt: "Refs-With-User: #2",
+			},
+			want: Footer{Tag: "Refs-With-User", Title: "#2"},
+		},
+		{
+			name: "hash footer paragraph",
+			args: args{
+				txt: "Close #1, #2",
+			},
+			want: Footer{Tag: "Close", Title: "#1, #2"},
+		},
+		{
+			name: "invalid hash footer paragraph with # prefix",
+			args: args{
+				txt: "#Close #1, #2",
+			},
+			want: Footer{Tag: "", Title: "#Close #1, #2"},
+		},
+		{
+			name: "invalid hash footer paragraph without # prefix",
+			args: args{
+				txt: "Close 1, #2",
+			},
+			want: Footer{Tag: "", Title: "Close 1, #2"},
+		},
+		{
+			name: "BREAKING CHANGE footer paragraph",
+			args: args{
+				txt: "BREAKING CHANGE: this is a breaking change",
+			},
+			want: Footer{Tag: "BREAKING CHANGE", Title: "this is a breaking change"},
+		},
+		{
+			name: "invalid BREAKING CHANGES footer paragraph",
+			args: args{
+				txt: "BREAKING CHANGES: this is a breaking change",
+			},
+			want: Footer{Tag: "", Title: "BREAKING CHANGES: this is a breaking change"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := paseFooterParagraph(tt.args.txt); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("paseFooterParagraph() = %v, want %v", got, tt.want)
 			}
 		})
 	}
